a15879acad165b7453dc8f55f25acf843cd48b3f
Jusan Magno <jusanmagno@gmail.com>
2022-07-06 00:40:45 -0300

    feat: configuração adicionais de port

--- index.js
+++ index.js
@@ -23,4 +23,4 @@ app.use('/api/auth', authRouter.routes);
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log('NARP is listening on http://localhost:80'));
+app.listen(port, () => console.log(`NARP is listening on http://localhost:${port}`));
